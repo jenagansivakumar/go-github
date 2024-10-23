f407a144ad8fd610ded3b760ec329a6772adca3b
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-23 18:53:51 +0200

    started creating Form data

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+)
 
 func main() {
 	var clientID string
 	fmt.Print("Please enter your Github Client ID: ")
 	fmt.Scan(&clientID)
 	fmt.Println(clientID)
+
+	data := url.Values{}
+
+	githubUrl := "https://github.com/login/device/code"
+
+	http.PostForm(githubUrl, data)
 
 }
