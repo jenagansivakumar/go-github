cc37fe96b507b85576c84d810ae81db5f8176297
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-23 15:50:25 +0200

    receiving 200 status

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	response, err := http.Get("https://api.github.com")
+	if err != nil {
+		fmt.Println("Error!")
+	}
+	fmt.Printf(response.Status)
 }
